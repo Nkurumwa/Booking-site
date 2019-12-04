f1612d308f222292ec93a28e459d78c2100056aa
Nkurumwa <nkurumwa51@gmail.com>
2019-12-04 11:50:35 +0300

    adds database to settings

--- hotel/settings.py
+++ hotel/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'hotel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hotel',
+        'USER': 'moringa',
+    'PASSWORD':'ishmael',
     }
 }
+
 
 
 # Password validation
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
